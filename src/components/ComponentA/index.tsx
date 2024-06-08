5b5ca53f08683c583409a82bd579a0bd1c6becc2
Elzar <chynarbekov07@gmail.com>
2024-06-08 15:04:47 +0600

    ui-jasoo

--- src/components/ComponentA/index.tsx
+++ src/components/ComponentA/index.tsx
@@ -1,6 +1,37 @@
 export const ComponnetA = () => {
-  return (
-  <div>
-    <h1>Component A</h1>
-  </div>);
-};
+	return (
+		<center>
+			{' '}
+			<div className='main-block'>
+				<div className='wordleGame-content-block'></div>
+				<div className='wordleGame-content-block'></div>
+				<div className='wordleGame-content-block'></div>
+				<div className='wordleGame-content-block'></div>
+				<div className='wordleGame-content-block'></div>
+				<div className='wordleGame-content-block'></div>
+				<div className='wordleGame-content-block'></div>
+				<div className='wordleGame-content-block'></div>
+				<div className='wordleGame-content-block'></div>
+				<div className='wordleGame-content-block'></div>
+				<div className='wordleGame-content-block'></div>
+				<div className='wordleGame-content-block'></div>
+				<div className='wordleGame-content-block'></div>
+				<div className='wordleGame-content-block'></div>
+				<div className='wordleGame-content-block'></div>
+				<div className='wordleGame-content-block'></div>
+				<div className='wordleGame-content-block'></div>
+				<div className='wordleGame-content-block'></div>
+				<div className='wordleGame-content-block'></div>
+				<div className='wordleGame-content-block'></div>
+				<div className='wordleGame-content-block'></div>
+				<div className='wordleGame-content-block'></div>
+				<div className='wordleGame-content-block'></div>
+				<div className='wordleGame-content-block'></div>
+				<div className='wordleGame-content-block'></div>
+			</div>
+			<div className='main-block-text'>
+				<h1>Wordle #195</h1> <h1>08/05/2024</h1>
+			</div>
+		</center>
+	)
+}
